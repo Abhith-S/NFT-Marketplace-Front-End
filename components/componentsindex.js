e15a089c8b7542afdd08d0343b30e2931b95ffd3
Abhith-S <abhithshaji@gmail.com>
2023-03-14 22:35:16 +0530

    subscribe component completed

--- components/componentsindex.js
+++ components/componentsindex.js
@@ -4,4 +4,7 @@ import NavBar from "./NavBar/NavBar";
 import HeroSection from "./HeroSection/HeroSection";
 import Service from "./Service/Service";
 import BigNFTSlider from "./BigNFTSlider/BigNFTSlider";
-export {Button, Footer, NavBar , HeroSection, Service, BigNFTSlider};
+import Subscribe from "./Subscribe/Subscribe";
+
+
+export {Button, Footer, NavBar , HeroSection, Service, BigNFTSlider, Subscribe};
